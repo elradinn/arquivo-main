147fda6e4de9649e957c332a457ef7e22e8ae949
elradinn <johnalfredalfonso.dev@gmail.com>
2024-11-11 22:53:02 +0800

    build

---
+++ public/build/assets/Dashboard-CL2nlteO.js
@@ -0,0 +1,36 @@
+import{j as e,P as g,y as u,Y as _,a as f}from"./app-Bi-kKfSQ.js";import{A as x}from"./Authenticated-C_lZwoKT.js";import{Y as j}from"./index-BALgSnzC.js";import{G as i,d as y}from"./NotificationMenu-C937_Mzw.js";import{G as p}from"./Grid-C2RbmrfU.js";import{S as k}from"./SimpleGrid-CY-TsJrP.js";import{T as l}from"./OfficeLogo-2ZaB1g-B.js";import{c as r}from"./IconUser-DGEFyDH_.js";import{I as M}from"./IconFile-BRMhlduA.js";import{S as w}from"./StateBadge-r0ACx74v.js";import{S as n}from"./Stack-D9KjGeyZ.js";import{B as I}from"./Button-1yfY3Uus.js";import"./TextInput-D9HeubEm.js";import"./InputBase-UL63YNSc.js";import"./Popover-DE1Ul7Ry.js";import"./use-uncontrolled-BwiKmmNw.js";import"./get-sorted-breakpoints-Cse3ButY.js";import"./Checkbox-3zVWk8bN.js";import"./CheckIcon-DsGROgfh.js";import"./get-base-value-JqT_q0U7.js";import"./Badge-BddBrT8H.js";/**
+ * @license @tabler/icons-react v3.11.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var b=r("outline","arrow-right","IconArrowRight",[["path",{d:"M5 12l14 0",key:"svg-0"}],["path",{d:"M13 18l6 -6",key:"svg-1"}],["path",{d:"M13 6l6 6",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.11.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var P=r("outline","file-check","IconFileCheck",[["path",{d:"M14 3v4a1 1 0 0 0 1 1h4",key:"svg-0"}],["path",{d:"M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z",key:"svg-1"}],["path",{d:"M9 15l2 2l4 -4",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.11.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var S=r("outline","file-description","IconFileDescription",[["path",{d:"M14 3v4a1 1 0 0 0 1 1h4",key:"svg-0"}],["path",{d:"M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z",key:"svg-1"}],["path",{d:"M9 17h6",key:"svg-2"}],["path",{d:"M9 13h6",key:"svg-3"}]]);/**
+ * @license @tabler/icons-react v3.11.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var C=r("outline","file-x","IconFileX",[["path",{d:"M14 3v4a1 1 0 0 0 1 1h4",key:"svg-0"}],["path",{d:"M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z",key:"svg-1"}],["path",{d:"M10 12l4 4m0 -4l-4 4",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.11.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var R=r("outline","message-check","IconMessageCheck",[["path",{d:"M8 9h8",key:"svg-0"}],["path",{d:"M8 13h6",key:"svg-1"}],["path",{d:"M10.99 19.206l-2.99 1.794v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v6",key:"svg-2"}],["path",{d:"M15 19l2 2l4 -4",key:"svg-3"}]]);/**
+ * @license @tabler/icons-react v3.11.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var D=r("outline","message-x","IconMessageX",[["path",{d:"M8 9h8",key:"svg-0"}],["path",{d:"M8 13h6",key:"svg-1"}],["path",{d:"M13 18l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v6",key:"svg-2"}],["path",{d:"M22 22l-5 -5",key:"svg-3"}],["path",{d:"M17 22l5 -5",key:"svg-4"}]]);/**
+ * @license @tabler/icons-react v3.11.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var z=r("outline","message","IconMessage",[["path",{d:"M8 9h8",key:"svg-0"}],["path",{d:"M8 13h6",key:"svg-1"}],["path",{d:"M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z",key:"svg-2"}]]);const A="_value_10x7p_1",F="_diff_10x7p_5",G="_doubleHeight_10x7p_11",H="_card_10x7p_15",N="_title_10x7p_23",s={value:A,diff:F,doubleHeight:G,card:H,title:N},T={review_pending:z,review_accepted:R,review_rejected:D,approval_pending:M,approval_accepted:P,approval_rejected:C,total_documents:S};function $({dashboard:t}){const o=[{title:"Review Accepted",icon:"review_accepted",value:t.number_of_review_accepted,color:"teal",statusParam:"reviewal_accepted"},{title:"Review Pending",icon:"review_pending",value:t.number_of_review_pending,color:"orange",statusParam:"reviewal_pending"},{title:"Review Rejected",icon:"review_rejected",value:t.number_of_review_rejected,color:"red",statusParam:"reviewal_rejected"},{title:"Approval Accepted",icon:"approval_accepted",value:t.number_of_approval_accepted,color:"green",statusParam:"approval_accepted"},{title:"Approval Pending",icon:"approval_pending",value:t.number_of_approval_pending,color:"orange",statusParam:"approval_pending"},{title:"Approval Rejected",icon:"approval_rejected",value:t.number_of_approval_rejected,color:"red",statusParam:"approval_rejected"},{title:"Total Documents",icon:"total_documents",value:t.number_of_documents,color:"violet",statusParam:null}];o.find(a=>a.title==="Total Documents");const c=o.filter(a=>a.title!=="Total Documents"),d=(a,v=!1)=>{const h=T[a.icon],m=()=>{a.statusParam&&u.visit(`/dashboard/reports?document_status=${a.statusParam}`)};return e.jsxs(g,{withBorder:!0,p:"lg",radius:"md",onClick:m,shadow:"xs",style:{cursor:a.statusParam?"pointer":"default"},className:`${s.card} ${v?s.doubleHeight:""}`,children:[e.jsx(i,{justify:"left",mb:"sm",children:e.jsx("div",{style:{backgroundColor:`var(--mantine-color-${a.color}-light)`,borderRadius:"50%",padding:"10px",display:"flex",justifyContent:"center",alignItems:"center"},children:e.jsx(h,{className:s.icon,size:"1.4rem",color:`var(--mantine-color-${a.color}-light-color)`,height:32,width:32})})}),e.jsx(l,{size:"sm",c:"dimmed",className:s.title,children:a.title}),e.jsx(i,{align:"center",mt:10,children:e.jsx(l,{className:s.value,size:"lg",children:a.value})})]},a.title)};return e.jsx("div",{className:s.root,children:e.jsx(p,{children:e.jsx(p.Col,{children:e.jsx(k,{cols:{base:1,lg:3},children:c.map(a=>d(a))})})})})}function le({dashboard:t}){return e.jsxs(x,{children:[e.jsx(_,{title:"Dashboard"}),e.jsxs(n,{px:8,gap:48,py:8,children:[e.jsxs(n,{gap:24,children:[e.jsxs(i,{align:"center",justify:"space-between",children:[e.jsx(l,{component:"h2",size:"xl",fw:600,color:"gray.8",children:"Dashboard"}),e.jsx(I,{component:f,href:"/dashboard/reports",variant:"subtle",color:"blue",rightSection:e.jsx(b,{size:16}),children:"Go to Reports"})]}),e.jsx($,{dashboard:t})]}),e.jsxs(n,{gap:24,children:[e.jsx(l,{c:"gray.7",fw:500,children:"Recently Uploaded Documents"}),e.jsx(j,{textSelectionDisabled:!0,columns:[{accessor:"name",render:({name:o,status:c})=>e.jsxs(i,{align:"center",gap:12,children:[e.jsx(y,{mime:"application/pdf",isFolder:!1,approvalStatus:c}),e.jsx("span",{children:o})]})},{accessor:"status",render:({status:o})=>e.jsx(w,{state:o})},{accessor:"date_uploaded",title:"Date Uploaded"}],records:t.recently_uploaded_documents,highlightOnHover:!0,verticalSpacing:"lg",horizontalSpacing:"xl"})]})]})]})}export{le as default};
--- public/build/assets/StateBadge-DcKDHK1I.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DlxKuYZb.js";import{g as t}from"./NotificationMenu-Ce37Wr51.js";import{B as a}from"./Badge-B2L46iXP.js";const m=({state:o})=>r.jsx(a,{color:t(o),variant:"light",children:o??"None"});export{m as S};
