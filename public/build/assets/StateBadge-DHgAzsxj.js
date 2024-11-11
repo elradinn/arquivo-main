27247ad62e001006f78398e8a3989ee886257837
elradinn <johnalfredalfonso.dev@gmail.com>
2024-11-11 12:01:48 +0800

    build

--- public/build/assets/StateBadge-DHgAzsxj.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DHbb7XZB.js";import{g as t}from"./get-color-status-DWf4oBUm.js";import{B as a}from"./Badge-By5z6ASX.js";const m=({state:o})=>r.jsx(a,{color:t(o),variant:"light",children:o??"None"});export{m as S};
--- public/build/assets/use-document-properties-keVFbWrw.js
+++
@@ -1,1 +0,0 @@
-import{y as t}from"./app-DHbb7XZB.js";function u(){return{openDocument:o=>{t.visit(route("document.show",{document:o}))}}}export{u};
