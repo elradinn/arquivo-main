f0c27fbb7ad7f922ca98c4c2173d34caf3ff5969
elradinn <johnalfredalfonso.dev@gmail.com>
2024-11-13 21:16:30 +0800

    build

--- public/build/assets/StateBadge-r0ACx74v.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-Bi-kKfSQ.js";import{g as t}from"./NotificationMenu-C937_Mzw.js";import{B as a}from"./Badge-BddBrT8H.js";const m=({state:o})=>r.jsx(a,{color:t(o),variant:"light",children:o??"None"});export{m as S};
--- public/build/assets/use-fetch-metadata-CCUa2s-n.js
+++
@@ -1,1 +0,0 @@
-import{b as a,c as s}from"./app-Bi-kKfSQ.js";function c(){const[e,r]=a.useState([]);return a.useEffect(()=>{s.get(route("metadata.fetch")).then(t=>{r(t.data.metadata)}).catch(t=>{console.error("Error fetching metadata:",t)})},[]),{metadataList:e}}export{c as u};
