e8ee4ec142ef081a34b3a7319533b3a4ee21802b
elradinn <johnalfredalfonso.dev@gmail.com>
2024-11-20 04:45:36 +0800

    build

--- public/build/assets/use-document-properties-BZjTl5C9.js
+++
@@ -1,1 +0,0 @@
-import{y as t}from"./app-Dj8oY4Sv.js";function u(){return{openDocument:o=>{t.visit(route("document.show",{document:o}))}}}export{u};
---
+++ public/build/assets/use-document-properties-OVUJVk49.js
@@ -0,0 +1,1 @@
+import{y as t}from"./app-Bk6YQbjT.js";function u(){return{openDocument:o=>{t.visit(route("document.show",{document:o}))}}}export{u};
