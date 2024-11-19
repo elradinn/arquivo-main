38653f8f5784f55b2b65e4485efddf8c8f81e280
elradinn <johnalfredalfonso.dev@gmail.com>
2024-11-19 23:29:09 +0800

    hmm

--- app/Modules/Dashboard/Helpers/DocumentStatusHelper.php
+++ app/Modules/Dashboard/Helpers/DocumentStatusHelper.php
@@ -5,12 +5,12 @@ namespace Modules\Dashboard\Helpers;
 class DocumentStatusHelper
 {
     protected static $statusMap = [
-        'reviewal_pending' => 'Modules\DocumentApproval\States\DocumentReviewalPending',
-        'reviewal_accepted' => 'Modules\DocumentApproval\States\DocumentReviewalAccepted',
-        'reviewal_rejected' => 'Modules\DocumentApproval\States\DocumentReviewalRejected',
-        'approval_pending' => 'Modules\DocumentApproval\States\DocumentApprovalPending',
-        'approval_accepted' => 'Modules\DocumentApproval\States\DocumentApprovalAccepted',
-        'approval_rejected' => 'Modules\DocumentApproval\States\DocumentApprovalRejected',
+        'reviewal_pending' => 'Modules\Document\States\DocumentReviewalPending',
+        'reviewal_accepted' => 'Modules\Document\States\DocumentReviewalAccepted',
+        'reviewal_rejected' => 'Modules\Document\States\DocumentReviewalRejected',
+        'approval_pending' => 'Modules\Document\States\DocumentApprovalPending',
+        'approval_accepted' => 'Modules\Document\States\DocumentApprovalAccepted',
+        'approval_rejected' => 'Modules\Document\States\DocumentApprovalRejected',
     ];
 
     public static function getStatusClass(string $status): ?string
