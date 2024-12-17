0dfb72b9622f84e4fbaab13e2f29e9851272723e
elradinn <johnalfredalfonso.dev@gmail.com>
2024-12-18 06:57:10 +0800

    build

---
+++ public/build/assets/StateBadge-Pv7ayFmr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-Bb5USZrt.js";import{g as t}from"./NotificationMenu-DIIl_Q3g.js";import{B as a}from"./Badge-D2fU9ZDK.js";const m=({state:o})=>r.jsx(a,{color:t(o),variant:"light",children:o??"None"});export{m as S};
